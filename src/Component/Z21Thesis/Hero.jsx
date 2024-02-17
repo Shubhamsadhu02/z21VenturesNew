e0e818b7b5c4bbd1491a4e9cb9ce25b8d14e5bd7
mailtoravi437 <mailtoravi437@gmail.com>
2024-02-17 15:10:18 +0530

    Z21ThesisHero

--- src/Component/Z21Thesis/Hero.jsx
+++ src/Component/Z21Thesis/Hero.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Z21ThesisHero from "./images/Z21ThesisHero.svg";
 
 export default function Hero() {
@@ -8,7 +7,7 @@ export default function Hero() {
       style={{ backgroundImage: `url(${Z21ThesisHero})`, marginTop: "-60px" }}>
       <div className="absolute inset-0 "></div>
       <div className="container">
-        <div className="absolute bottom-0 right-0 pb-8 pr-8 md:pb-16 md:pr-16 lg:pb-24 lg:pr-2">
+        <div className="absolute right-2 bottom-12">
           <p className="text-4xl md:text-8xl max-w-xs md:max-w-sm lg:max-w-md text-white font-normal">
             z21 <span className=" font-kalnia">thesis</span>
           </p>
