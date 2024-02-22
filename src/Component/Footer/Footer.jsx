b6d1861b04f9abbc140ace8bf30216fc37d875ae
shubham3000 <shubhamsadhu02@gmail.com>
2024-02-22 23:00:24 +0530

    footer animation

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,50 +1,70 @@
 import React from "react";
 import FooterBG from "../Footer/Footer.png";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import { textVariants, fadeInAnimationCompanies } from "../../FramerAnimation/Framer";
 
 export default function Footer() {
+  const footerList = [
+    {
+      name: "About Us",
+      link: "/aboutus"
+    },
+    {
+      name: "z21 Thesis",
+      link: "/thesis"
+    },
+    {
+      name: "Portfolio",
+      link: "/portfolio"
+    },
+    {
+      name: "Community",
+      link: "/community"
+    },
+    {
+      name: "Blogs",
+      link: "/blogs"
+    }
+  ]
   return (
     <section
       className="w-screen pt-12 md:pt-24 border-t-2 md:border-[#CED0D4] "
       style={{ background: "EFEFF1" }}>
       <div className="container relative">
         <div className="w-full flex md:justify-end px-10 md:px-20 mb-16 md:mb-48">
-          <ul className="md:flex md:justify-between w-2/3">
-            <Link to={"/aboutus"}>
-              <li className="border-l-2 border-[#DE5126] pl-4 text-base md:text-xl font-arimo font-semibold mb-3">
-                About us
-              </li>
-            </Link>
-            <Link to={"/thesis"}>
-              <li className="border-l-2 border-[#DE5126] pl-4 text-base md:text-xl font-arimo font-semibold mb-3">
-                z21 Thesis
-              </li>
-            </Link>
-            <Link to={"/portfolio"}>
-              <li className="border-l-2 border-[#DE5126] pl-4 text-base md:text-xl font-arimo font-semibold mb-3">
-                Portfolio
-              </li>
-            </Link>
-            <Link to={"/community"}>
-              <li className="border-l-2 border-[#DE5126] pl-4 text-base md:text-xl font-arimo font-semibold mb-3">
-                Community
-              </li>
-            </Link>
-            <Link to={"/blogs"}>
-              <li className="border-l-2 border-[#DE5126] pl-4 text-base md:text-xl font-arimo font-semibold mb-3">
-                Blogs
-              </li>
-            </Link>
-          </ul>
+          <motion.ul className="md:flex md:justify-between w-2/3">
+            {
+              footerList.map((item, index) => {
+                return (
+                  <Link to={item.link}>
+                    <motion.li className="border-l-2 border-[#DE5126] pl-4 text-base md:text-xl font-arimo font-semibold mb-3"
+                      variants={{ ...fadeInAnimationCompanies }}
+                      initial="initial"
+                      whileInView="animate"
+                      custom={index}>
+                      {item.name}
+                    </motion.li>
+                  </Link>
+                )
+              })
+            }
+          </motion.ul>
         </div>
         <div className=" absolute right-0 bottom-20 md:bottom-1/3 px-10 md:px-20">
-          <p className="text-base md:text-xl font-arimo font-semibold">
+          <motion.p className="text-base md:text-xl font-arimo font-semibold"
+            initial="hidden"
+            whileInView="visible"
+            variants={{ ...textVariants }}>
             © Copyright | Z21 Ventures
-          </p>
+          </motion.p>
         </div>
-        <div className="mb-3">
+        <motion.div className="mb-3"
+          initial="hidden"
+          whileInView="visible"
+          variants={{ ...textVariants }}>
           <img src={FooterBG} alt="Footer" />
-        </div>
+        </motion.div>
       </div>
     </section>
   );
