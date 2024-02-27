fa0e70580fcccbb2fd7093934f12c6d47593c874
mailtoravi437 <mailtoravi437@gmail.com>
2024-02-27 18:51:57 +0530

    Position and email fixed

--- src/Component/AboutUs/OurTeam.jsx
+++ src/Component/AboutUs/OurTeam.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import TeamBg from "../AboutUs/images/OurTeam/Bg.png";
 
 import Abhinav from "../AboutUs/images/OurTeam/Abhinav.png";
@@ -8,71 +8,81 @@ import Dibyendu from "../AboutUs/images/OurTeam/Dibyendu.png";
 import Jyotika from "../AboutUs/images/OurTeam/Jyotika.png";
 import Raj from "../AboutUs/images/OurTeam/Raj.png";
 import Tony from "../AboutUs/images/OurTeam/Tony.png";
-import { motion } from 'framer-motion';
-import { textVariants } from '../../FramerAnimation/Framer';
-
+import { motion } from "framer-motion";
+import { textVariants } from "../../FramerAnimation/Framer";
 
 export default function OurTeam() {
-    let ourTeam = [
-        {
-            image: Raj,
-            name: "Raj Shekhar Singh",
-            designation: "General Partner"
-        },
-        {
-            image: Abhinav,
-            name: "Abhinav Shashank",
-            designation: "General Partner"
-        },
-        {
-            image: Jyotika,
-            name: "Jyotika Gupta",
-            designation: "Partner"
-        },
-        {
-            image: Ankit,
-            name: "Ankit Bhangar",
-            designation: "Venture Advisor"
-        },
-        {
-            image: Tony,
-            name: "Tony Hu",
-            designation: "Legal Counsel"
-        },
-        {
-            image: Anand,
-            name: "Anand Akela",
-            designation: "Venture Advisor"
-        },
-        {
-            image: Dibyendu,
-            name: "Dibyendu Rana",
-            designation: "Venture Advisor"
-        }
-    ]
-    return (
-        <section className="w-screen py-12 md:py-24 px-10 md:px-20 bg-no-repeat bg-cover bg-center" style={{ backgroundImage: `url(${TeamBg})` }}>
-            <div className="container">
-                <motion.h1 className="text-black text-4xl md:text-8xl font-semibold mb-16 font-arimo"
-                    initial="hidden"
-                    whileInView="visible"
-                    variants={{ ...textVariants }}>
-                    Our Team
-                </motion.h1>
-                <div className="flex flex-wrap md:grid md:grid-cols-3 gap-[50px] mt-16">
-                    {
-                        ourTeam.map((item, index) => {
-                            return (
-                                <div key={index} className="group p-8 transition duration-300 ease-in-out hover:shadow-lg hover:p-5 rounded-md hover:scale-105">
-                                    <img src={item.image} alt="" className='grayscale group-hover:grayscale-0 w-96' />
-                                    <h4 className='text-xl md:text-3xl font-bold font-arimo mt-6 border-b-[1px] border-[#CED0D4] pb-4 group-hover:text-[#DE5126]'>{item.name}</h4>
-                                    <p className='text-base md:text-xl font-normal text-[#60646C] mt-4'>{item.designation}</p>
-                                </div>
-                            )
-                        })
-                    }
-                </div>
-            </div>
-        </section>
-    )
+  let ourTeam = [
+    {
+      image: Raj,
+      name: "Raj Shekhar Singh",
+      designation: "Co-founder & General Partner",
+    },
+    {
+      image: Abhinav,
+      name: "Abhinav Shashank",
+      designation: "Co-founder & Investment Advisor",
+    },
+    {
+      image: Jyotika,
+      name: "Jyotika Gupta",
+      designation: "Partner",
+    },
+    {
+      image: Ankit,
+      name: "Ankit Bhangar",
+      designation: "Partner",
+    },
+    {
+      image: Tony,
+      name: "Tony Hu",
+      designation: "Legal Counsel",
+    },
+    {
+      image: Anand,
+      name: "Anand Akela",
+      designation: "Venture Advisor",
+    },
+    {
+      image: Dibyendu,
+      name: "Dibyendu Rana",
+      designation: "Venture Advisor",
+    },
+  ];
+  return (
+    <section
+      className="w-screen py-12 md:py-24 px-10 md:px-20 bg-no-repeat bg-cover bg-center"
+      style={{ backgroundImage: `url(${TeamBg})` }}>
+      <div className="container">
+        <motion.h1
+          className="text-black text-4xl md:text-8xl font-semibold mb-16 font-arimo"
+          initial="hidden"
+          whileInView="visible"
+          variants={{ ...textVariants }}>
+          Our Team
+        </motion.h1>
+        <div className="flex flex-wrap md:grid md:grid-cols-3 gap-[50px] mt-16">
+          {ourTeam.map((item, index) => {
+            return (
+              <div
+                key={index}
+                className="group p-8 transition duration-300 ease-in-out hover:shadow-lg hover:p-5 rounded-md hover:scale-105">
+                <img
+                  src={item.image}
+                  alt=""
+                  className="grayscale group-hover:grayscale-0 w-96"
+                />
+                <h4 className="text-xl md:text-3xl font-bold font-arimo mt-6 border-b-[1px] border-[#CED0D4] pb-4 group-hover:text-[#DE5126]">
+                  {item.name}
+                </h4>
+                <p className="text-base md:text-xl font-normal text-[#60646C] mt-4">
+                  {item.designation}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
 }
