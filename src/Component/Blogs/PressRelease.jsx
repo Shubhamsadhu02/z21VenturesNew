7bfba780df731333871bbf4ffc2039cdb03d268a
shubham3000 <shubhamsadhu02@gmail.com>
2024-05-27 14:47:38 +0530

    press release updated

--- src/Component/Blogs/PressRelease.jsx
+++ src/Component/Blogs/PressRelease.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { MdArrowOutward } from 'react-icons/md';
 import { Link } from 'react-router-dom';
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -8,75 +7,100 @@ import "swiper/css/pagination";
 import { Pagination } from "swiper/modules";
 import { Autoplay } from "swiper/modules";
 
+import { motion } from 'framer-motion';
+
 import Frame from './images/frame.png';
+import Announcement from './images/Announcement icon.png';
+import { GoArrowRight } from 'react-icons/go';
 
 export default function PressRelease() {
 
     const slide = [
         {
-
-            header: "z21 Ventures has been a game changer for us",
-            description:
-                "z21 Ventures has been a game-changer for us. Their strategic investments and unwavering support propelled our startup to new heights. Truly a visionary partner!",
-            name: "Nikunj Bajaj | CEO - Truefoundary",
-            link: "https://www.linkedin.com/in/nikunj-bajaj-10476824/",
+            header: "Bay Area based VC fund z21 Ventures deploys it’s first fund in 25+ companies",
+            phase: "Phase-1 Announcement",
+            link: "",
         },
         {
-
-            header: "It has been an enriching experience",
-            description:
-                "Being part of the z21 community has been an enriching experience. The collaborative environment, coupled with z21's insightful guidance, has played a crucial role in our journey of innovation and success",
-            name: "Sushant Gupta | CEO - Meomind",
-            link: "https://www.linkedin.com/in/sushantgupta1/",
+            header: "Bay Area based VC fund z21 Ventures deploys it’s first fund in 25+ companies",
+            phase: "Phase-2 Announcement",
+            link: "",
         },
         {
-
-            header: "z21 Ventures has been a game changer for us",
-            description:
-                "z21 Ventures has been a game-changer for us. Their strategic investments and unwavering support propelled our startup to new heights. Truly a visionary partner!",
-            name: "Nikunj Bajaj | CEO - Truefoundary",
-            link: "https://www.linkedin.com/in/nikunj-bajaj-10476824/",
+            header: "Bay Area based VC fund z21 Ventures deploys it’s first fund in 25+ companies",
+            phase: "Phase-1 Announcement",
+            link: "",
         },
         {
-
-            header: "It has been an enriching experience",
-            description:
-                "Being part of the z21 community has been an enriching experience. The collaborative environment, coupled with z21's insightful guidance, has played a crucial role in our journey of innovation and success",
-            name: "Sushant Gupta | CEO - Meomind",
-            link: "https://www.linkedin.com/in/sushantgupta1/",
+            header: "Bay Area based VC fund z21 Ventures deploys it’s first fund in 25+ companies",
+            phase: "Phase-1 Announcement",
+            link: "",
         },
+
     ]
     return (
         <>
-            <section className='w-screen h-auto py-12 md:py-24 px-5 md:px-20 bg-[#DE5126]'>
+            <section className='w-screen h-auto py-12 md:py-24 px-5 bg-[#DE5126]'>
                 <div className="container">
                     <h2 className="text-white text-4xl md:text-6xl lg:text-8xl font-arimo font-semibold">
                         Press Release
                     </h2>
 
                     <div className="my-12 md:my-24">
-                        <Swiper slidesPerView={4}
-                            spaceBetween={30}
-                            // loop={true}
-                            centeredSlides={true}
-                            // modules={[Autoplay]}
-                            // autoplay={{
-                            //     delay: 3500,
-                            //     disableOnInteraction: true,
-                            //     infinite: true,
-                            // }}
-                            className="mySwiper">
+                        <Swiper
+                            modules={[Pagination]}
+                            pagination={{
+                                el: '.swiper-pagination',
+                                type: 'bullets',
+                                clickable: true,
+                            }}
+                            breakpoints={{
+                                500: {
+                                    slidesPerView: 2,
+                                    spaceBetween: 10,
+                                    centeredSlides: true,
+                                    pagination: {
+                                        dynamicBullets: true,
+                                        clickable: false,
+                                        bulletClass: 'press-bullet-class',
+                                        bulletActiveClass: 'press-bullet-active-class',
+                                        className: "press-bullet-active-class",
+                                    },
+                                },
+                                768: {
+                                    slidesPerView: 2,
+                                    spaceBetween: 30,
+                                    centeredSlides: true,
+                                    pagination: false
+                                },
+                                1024: {
+                                    slidesPerView: 2,
+                                    spaceBetween: 30,
+                                    centeredSlides: true,
+                                    pagination: false
+                                },
+                            }}
+                            className="mySwiper h-[560px] lg:h-auto press-custom-class">
                             {
                                 <div className="">
                                     {slide.map((ele, index) => {
                                         return (
                                             <SwiperSlide key={index}>
-                                                <div className="bg-white">
+                                                <div className="bg-white w-[300px] sm:w-[350px] lg:w-[455px] xl:w-[580px] overflow-hidden group">
                                                     <div className="">
-                                                        <img src={Frame} alt="" />
+                                                        <img src={Frame} alt="" className=' w-[580px] h-[230px] lg:h-[300px] xl:h-[440px] transform transition-transform duration-300 group-hover:scale-105' />
                                                     </div>
-                                                    <div className="">
-                                                        <p>Bay Area based VC fund z21 Ventures deploys it’s first fund in 25+ companies</p>
+                                                    <div className=" bg-black flex items-center justify-center p-2 rounded-r-full w-[210px] lg:w-[276px] relative bottom-6">
+                                                        <p className=' font-kalnia text-xs lg:text-base font-medium text-white mr-4'>{ele.phase}</p>
+                                                        <img src={Announcement} alt="" className=' w-6 h-6' />
+                                                    </div>
+                                                    <div className=" p-6 xl:p-12 flex flex-col justify-center">
+                                                        <p className=' font-arimo font-medium text-2xl lg:text-4xl overflow-hidden overflow-ellipsis whitespace-pre-line line-clamp-3'>{ele.header}</p>
+                                                        <Link to={ele.link}>
+                                                            <motion.div className=" bg-black group-hover:bg-[#DE5126] flex justify-center items-center w-12 h-12 lg:w-20 lg:h-20 rounded-full mt-8 p-3 lg:p-6 text-xl lg:text-3xl font-semibold text-white">
+                                                                <GoArrowRight />
+                                                            </motion.div>
+                                                        </Link>
                                                     </div>
                                                 </div>
                                             </SwiperSlide>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       screens: {
-        sm: "640px",
+        sm: "400px",
         md: "768px",
         lg: "1024px",
         xl: "1280px",
