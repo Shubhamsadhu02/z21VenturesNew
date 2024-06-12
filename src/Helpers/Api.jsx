a4444c83a128b66765a5b93af489b727cd5e0381
mailtoravi437 <mailtoravi437@gmail.com>
2024-06-12 23:47:04 +0530

    Event prefault

--- src/Helpers/Api.jsx
+++ src/Helpers/Api.jsx
@@ -3,80 +3,84 @@ var BASE_URL = "https://blog.z21.ventures/wp-json/wp/v2/";
 var POST = "posts";
 var COMMUNITY = "community";
 var fetchBlogs = async () => {
-    try{
-        var res = await fetch(BASE_URL+POST+"?per_page=100");
-        var json = await res.json();
-        return json;
-    }catch(error){
-        return [];
-    }
-    
-}
+  try {
+    var res = await fetch(BASE_URL + POST + "?per_page=100");
+    var json = await res.json();
+    return json;
+  } catch (error) {
+    return [];
+  }
+};
 
 var fetchSingleBlog = async (slug) => {
-    try{
-        var res = await fetch(BASE_URL+POST+"?slug="+slug);
-        var json = await res.json();
-        console.log(json);
-        if(json.length > 0){
-          return json[0];  
-        }else{
-            return false;
-        }
-    }catch(error){
-        return false;
+  try {
+    var res = await fetch(BASE_URL + POST + "?slug=" + slug);
+    var json = await res.json();
+    console.log(json);
+    if (json.length > 0) {
+      return json[0];
+    } else {
+      return false;
     }
-}
+  } catch (error) {
+    return false;
+  }
+};
 
 const fetchRelatedBlogs = async () => {
-    try{
-        var res = await fetch(BASE_URL+POST+"?per_page=100");
-        var json = await res.json();
-        if(json.length > 4){
-            let arr = [];
-            for(var i = 0; i<3; i++){
-                arr.push(json[Math.floor(Math.random() * json.length)]);
-            }
-            return arr;
-        }
-        return json;
-    }catch(error){
-        return [];
+  try {
+    var res = await fetch(BASE_URL + POST + "?per_page=100");
+    var json = await res.json();
+    if (json.length > 4) {
+      let arr = [];
+      for (var i = 0; i < 3; i++) {
+        arr.push(json[Math.floor(Math.random() * json.length)]);
+      }
+      return arr;
     }
-}
+    return json;
+  } catch (error) {
+    return [];
+  }
+};
 
 var fetchCommunities = async () => {
-    try{
-        var res = await fetch(BASE_URL+COMMUNITY+"?per_page=100");
-        var json = await res.json();
-        return json.reverse();;
-    }catch(error){
-        return [];
-    }
-    
-}
+  try {
+    var res = await fetch(BASE_URL + COMMUNITY + "?per_page=100");
+    var json = await res.json();
+    return json.reverse();
+  } catch (error) {
+    return [];
+  }
+};
 
-const handleFormSubmit = async (event) => {  
-    var formdata = new FormData();
-    formdata.append("email", document.getElementById("email"));
-    formdata.append("name", document.getElementById("fullName"));
-    formdata.append("description", document.getElementById("description"));
-    formdata.append("subject", document.getElementById("subject"));
-    formdata.append("nameLink", document.getElementById("nameLink"));
-    formdata.append("urlLink", document.getElementById("urlLink"));
-    formdata.append('attachment', document.getElementById("attachment"));
+const handleFormSubmit = async (event) => {
+  event.preventDefault();
+  var formdata = new FormData();
+  formdata.append("email", document.getElementById("email"));
+  formdata.append("name", document.getElementById("fullName"));
+  formdata.append("description", document.getElementById("description"));
+  formdata.append("subject", document.getElementById("subject"));
+  formdata.append("nameLink", document.getElementById("nameLink"));
+  formdata.append("urlLink", document.getElementById("urlLink"));
+  formdata.append("attachment", document.getElementById("attachment"));
 
-    var requestOptions = {
-    method: 'POST',
+  var requestOptions = {
+    method: "POST",
     body: formdata,
-    redirect: 'follow'
-    };
+    redirect: "follow",
+  };
 
-    fetch("z21.ventures/mail/index.php", requestOptions)
-    .then(response => response.body())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-}
+  fetch("z21.ventures/mail/index.php", requestOptions)
+    .then((response) => response.body())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
+};
 
-
-export { fetchBlogs, fetchSingleBlog, fetchRelatedBlogs, handleFormSubmit, fetchCommunities };
+export {
+  fetchBlogs,
+  fetchSingleBlog,
+  fetchRelatedBlogs,
+  handleFormSubmit,
+  fetchCommunities,
+};
