b948ba55a0d826c30a9a8b13abce79bb9a236740
shubham3000 <shubhamsadhu02@gmail.com>
2024-06-20 10:26:40 +0530

    not found

--- src/Partials/PageNotFound.jsx
+++ src/Partials/PageNotFound.jsx
@@ -1,12 +1,8 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom';
 import Error from "./error.png";
+import { Link } from 'react-router-dom';
 
 export default function PageNotFound() {
-    const navigate = useNavigate();
-    const handleBack = () => {
-        navigate(-1);
-    };
     return (
         <>
             <section className='h-screen w-screen bg-black bg-no-repeat bg-cover bg-center py-16 md:py-28 px-5 md:px-20'>
@@ -14,7 +10,7 @@ export default function PageNotFound() {
                     <div className="flex flex-col justify-center items-center">
                         <img src={Error} alt="" className=' md:w-2/5 h-auto px-8 mt-12 md:mt-0 grayscale' />
                         <h2 className='text-white text-xl md:text-7xl mt-7 font-arimo'>Page Not Found</h2>
-                        <h5 className='text-white text-xl mt-7 cursor-pointer underline font-kalnia' onClick={handleBack}>Go Back To Home</h5>
+                        <Link to={"/"}><h5 className='text-white text-xl mt-7 cursor-pointer underline font-kalnia' >Go Back To Home</h5></Link>
                     </div>
                 </div>
             </section>
